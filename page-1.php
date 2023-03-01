0d95da3db07548d420440d75542529e704a44dea
hannahabell0911 <96234964+hannahabell0911@users.noreply.github.com>
2023-03-01 16:42:45 +0000

    from html to php

--- page-1.php
+++ page-1.php
@@ -27,7 +27,7 @@ $statement->closeCursor();
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Site Title</a>
+        <a class="navbar-brand" href="index.php">Site Title</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -38,13 +38,13 @@ $statement->closeCursor();
           <span class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-1.html">Page 1</a>
+                <a class="nav-link" aria-current="page" href="page-1.php">Page 1</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-2.html">Page 2</a>
+                <a class="nav-link" aria-current="page" href="page-2.php">Page 2</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-3.html">Page 3</a>
+                <a class="nav-link" aria-current="page" href="page-3.php">Page 3</a>
               </li>
             </ul>
           </span>
@@ -54,18 +54,15 @@ $statement->closeCursor();
 
 <main class="container">
   <div class="starter-template text-center">
-    <h1>Page 1</h1>
-    <h1>Fashion List</h1>
+    <h1>Best-Sellers List</h1>
     <section>
-        <!-- display a table of products -->
         <table>
             <tr>
-                <th>Code</th>
                 <th>Name</th>
                 <th>Price</th>
                 <th>Date</th>
                 <th>Description</th>
-                <th>Delete</th>
+                <th>Image</th>
             </tr>
 
             <?php foreach ($fashion as $fashion) : ?>
