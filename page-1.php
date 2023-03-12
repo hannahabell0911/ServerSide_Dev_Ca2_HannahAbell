7604b9baf446b5cf4cb23f489cf06c8ab47f29ad
hannahabell0911 <96234964+hannahabell0911@users.noreply.github.com>
2023-03-12 07:35:48 +0000

    new updates

--- page-1.php
+++ page-1.php
@@ -8,45 +8,39 @@ $statement->execute();
 $fashion = $statement->fetchAll();
 $statement->closeCursor();
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 ?>
 
 <?php include 'includes/header.php';?>
-<link href="css/mystyle.css" rel="stylesheet">
+<link href="css/mystyle1.css" rel="stylesheet">
 <body>
 <main class="container">
 <div class="bg-light" id= "header">
       <h3 class="text-center">Golden</h3>
       <p class="text-center">Fashion starts here at golden</p>
     </div>
-  <div class="starter-template text-center">
-    <h1>Best-Sellers List</h1>
-    <section>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Date</th>
-                <th>Description</th>
-                <th>Image</th>
-            </tr>
 
+ 
+    <h1>Best-Sellers List</h1>
+    <div class="card-container">
             <?php foreach ($fashion as $fashion) : ?>
-            <tr>
-                <td><?php echo $fashion['fashionCode']; ?></td>
-                <td><?php echo $fashion['fashionName']; ?></td>
-                <td class="right"><?php echo $fashion['fashionPrice']; ?></td>
-                <td> <?php echo $fashion['fashionDate']; ?></td>
-                <td> <?php echo $fashion['fashionDescription']; ?></td>
-                <td><form action="delete_product.php" method="post">
-                <input type="hidden" name="fashion_id"
-                value="<?php echo $fashion['fashionID']; ?>">
-                <input type="submit" value="Delete">
-                </form></td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
-    </section>
-</main>
+              <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src= <?php echo $fashion['image_url']; ?> alt="Card image cap">
+  <div class="card-body">
+  <?php echo $fashion['fashionCode']; ?>
+    <h5 class="card-title"><?php echo $fashion['fashionName']; ?></h5>
+    <p class="card-text"><?php echo $fashion['fashionDescription']; ?></p>
+    <a href="#" class="btn btn-primary"><?php echo $fashion['fashionPrice'];?></a>
+    <a href="#" class="btn btn-primary"><?php echo $fashion['fashionDate'];?></a>
+  </div>
+              </div>
+      </div>
+ 
+  
+<?php endforeach; ?>
+ 
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
   </div>
 
