780304c57917a2e386ff3f5dc428441ffd0f5c0e
hannahabell0911 <96234964+hannahabell0911@users.noreply.github.com>
2023-03-13 13:48:29 +0000

    final upload

--- index.php
+++ index.php
@@ -7,54 +7,44 @@ $statement = $db->prepare($queryFashion);
 $statement->execute();
 $fashion = $statement->fetchAll();
 $statement->closeCursor();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 <?php include 'includes/header.php';?>
+<?php include 'includes/footer.php';?>
 <!DOCTYPE html>
 <html>
 
 <!-- the head section -->
 <head>
-    <title>Product Manager</title>
-    <link rel="stylesheet" type="text/css" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="css/mystyle.css" />
+    
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Product Manager</h1></header>
+<header><h1>Complain and Delete from your feed</h1></header>
 <main>
-    <h1>Product List</h1>
-    <section>
-        <!-- display a table of products -->
-        <table>
-            <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Date</th>
-                <th>Description</th>
-                <th>Delete</th>
-            </tr>
-
-            <?php foreach ($products as $product) : ?>
-            <tr>
-                <td><?php echo $product['productCode']; ?></td>
-                <td><?php echo $product['productName']; ?></td>
-                <td class="right"><?php echo $product['listPrice']; ?></td>
-                <td> <?php echo $product['productDate']; ?></td>
-                <td> <?php echo $product['productDescription']; ?></td>
-                <td><form action="delete_product.php" method="post">
-                <input type="hidden" name="product_id"
-                value="<?php echo $product['productID']; ?>">
-                <input type="submit" value="Delete">
-                </form></td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
+    <h1 class="product-list-title"></h1>
+    <section class="product-grid">
+        <!-- display a grid of products -->
+        <?php foreach ($fashion as $product) : ?>
+            <div class="product">
+                <img src="<?php echo $product['image_url']; ?>" alt="Product Image">
+                <div class="product-name"><?php echo $product['fashionName']; ?></div>
+                <div class="product-description"><?php echo $product['fashionDescription']; ?></div>
+                <div class="product-price">$<?php echo $product['fashionPrice']; ?></div>
+                <form action="delete_product.php" method="post">
+                    <input type="hidden" name="fashion_id" value="<?php echo $product['fashionID']; ?>">
+                    <input class="product-delete" type="submit" value="Delete">
+                </form>
+            </div>
+        <?php endforeach; ?>
     </section>
 </main>
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Product Manager, Inc.</p>
-</footer>
+
 </body>
 </html>
